bcadbc6b40ba44407301270a456073cce2f24890
Alaan07 <rakeshofficial007@gmail.com>
2024-09-26 01:04:17 +0530

    all frontend over except one employee page backend still remaining most pf common frontend logic is complete

--- src/Components/Dashbord.jsx
+++ src/Components/Dashbord.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import { useNavigate } from 'react-router-dom';
+import { FaSearch } from 'react-icons/fa'
 
 function Dashbord() {
 const navigate = useNavigate();
@@ -27,12 +28,13 @@ const handleLogoutClick = () =>{
 }
 const handleCreatClick = () => {
   document.querySelector('.welcomediv').style.display = "none"
+  document.querySelector('.createemployeediv').style.display = "flex"
 }
-
 
 
   return (
     <div className='dashboardPageContainer'>
+    {/* //*********************************************** navtop part */}
     <div className='topdiv'>
         <div className='logodiv'>
             <h2 className='logo'>Logo</h2>
@@ -66,32 +68,46 @@ const handleCreatClick = () => {
               <li><button className='createbtn' onClick={handleCreatClick}>Create Employee</button></li>
             </ul>
           </div>
+          <div className='searchdiv'>
+            <ul>
+              <li><button className='searchIcon'><FaSearch className='fasearch_icon'/></button></li>
+              <li><input className='searchInput'  type="text" placeholder='Enter search Keyword'/></li>
+              <li>
+                <select name="searchfilters" id="filters" className='searchoptions'>
+                  <option value="Name">Name</option>
+                  <option value="Email">Email</option>
+                  <option value="Contact">Contact</option>
+                </select>
+              </li>
+            </ul>
+          </div>
 
-
+{/* //*********************************************** create part */}
           <div className='createemployeediv'>
             <form>
                 <table className='createTable'>
+                <thead className='tablehead'><tr><td colSpan={3}>Create Employee</td></tr></thead>
                   <tbody className='createtablebody'>
                       <tr className='createtablerow'>
-                          <td>Name </td>
+                          <td className='leftlabel'>Name </td>
                           <td>:</td>
-                          <td><input type="text" className="inputbox" placeholder='Enter your name'/></td>
+                          <td><input type="text" className="createinputs" placeholder='Enter your name'/></td>
                       </tr>
                       <tr className='createtablerow'>
-                          <td>Email </td>
+                          <td className='leftlabel'>Email </td>
                           <td>:</td>
-                          <td><input type="email" className="inputbox" placeholder='Enter your email'/></td>
+                          <td><input type="email" className="createinputs" placeholder='Enter your email'/></td>
                       </tr>
                       <tr className='createtablerow'>
-                          <td>Mobile No </td>
+                          <td className='leftlabel'>Mobile No </td>
                           <td>:</td>
-                          <td><input type="number" className="inputbox" placeholder='Enter your number' maxLength = {10}/></td>
+                          <td><input type="contact" className="createinputs" placeholder='Enter your number' maxLength = {10}/></td>
                       </tr>
                       <tr className='createtablerow'>
-                          <td>Designation </td>
+                          <td className='leftlabel'>Designation </td>
                           <td>:</td>
                           <td>
-                            <select name="designation" id="designation">
+                            <select name="designation" id="designation" className='createselect'>
                               <option value="HR">HR</option>
                               <option value="Manager">Manager</option>
                               <option value="sales">sales</option>
@@ -99,26 +115,37 @@ const handleCreatClick = () => {
                           </td>
                       </tr>
                       <tr className='createtablerow'>
-                          <td>Gender </td>
+                          <td className='leftlabel'>Gender </td>
                           <td>:</td>
-                          <td>
-                            <input type="radio"/><label htmlFor="male">male</label> 
-                            <input type="radio"/><label htmlFor="female">female</label>
+                          <td className='genderradio'>
+                            <div className='optionitems'>
+                               <input type="radio"/><label htmlFor="male">MALE</label>   
+                            </div>
+                            <div className='optionitems'>
+                               <input type="radio"/><label htmlFor="female">FEMALE</label>
+                            </div>
+                            
                           </td>
                       </tr>
                       <tr className='createtablerow'>
-                          <td>Course </td>
+                          <td className='leftlabel'>Course </td>
                           <td>:</td>
-                          <td>
-                            <input type="checkbox"/> <label htmlFor="MCA">MCA </label>
-                            <input type="checkbox"/> <label htmlFor="BCA">BCA </label>
-                            <input type="checkbox"/> <label htmlFor="BSC">BSC </label>
+                          <td className='coursecheckbox'>
+                              <div className='optionitems'>
+                                <input type="checkbox"/> <label htmlFor="MCA">MCA </label>
+                              </div>
+                              <div className='optionitems'>
+                                <input type="checkbox"/> <label htmlFor="BCA">BCA </label>
+                              </div>
+                              <div className='optionitems'>
+                                  <input type="checkbox"/> <label htmlFor="BSC">BSC </label>
+                              </div>
                           </td>
                       </tr>
                       <tr className='createtablerow'>
-                          <td>Img Upload </td>
+                          <td className='leftlabel'>Img Upload </td>
                           <td>:</td>
-                          <td><input type="file" accept='.png, .jpg'/></td>
+                          <td className='filetd'><input className="loadfile" type="file" accept='.png, .jpg'/></td>
                       </tr>
 
 
@@ -130,7 +157,87 @@ const handleCreatClick = () => {
             </form>
           </div>
 
+{/* //*********************************************** update part */}
+          <div className='updateemployeediv'>
+            <form>
+                <table className='createTable'>
+                <thead className='tablehead'><tr><td colSpan={3}>Update Employee</td></tr></thead>
+                  <tbody className='createtablebody'>
+                      <tr className='createtablerow'>
+                          <td className='leftlabel'>Name </td>
+                          <td>:</td>
+                          <td><input type="text" className="createinputs" placeholder='Enter your name'/></td>
+                      </tr>
+                      <tr className='createtablerow'>
+                          <td className='leftlabel'>Email </td>
+                          <td>:</td>
+                          <td><input type="email" className="createinputs" placeholder='Enter your email'/></td>
+                      </tr>
+                      <tr className='createtablerow'>
+                          <td className='leftlabel'>Mobile No </td>
+                          <td>:</td>
+                          <td><input type="contact" className="createinputs" placeholder='Enter your number' maxLength = {10}/></td>
+                      </tr>
+                      <tr className='createtablerow'>
+                          <td className='leftlabel'>Designation </td>
+                          <td>:</td>
+                          <td>
+                            <select name="designation" id="designation" className='createselect'>
+                              <option value="HR">HR</option>
+                              <option value="Manager">Manager</option>
+                              <option value="sales">sales</option>
+                            </select>
+                          </td>
+                      </tr>
+                      <tr className='createtablerow'>
+                          <td className='leftlabel'>Gender </td>
+                          <td>:</td>
+                          <td className='genderradio'>
+                            <div className='optionitems'>
+                               <input type="radio"/><label htmlFor="male">MALE</label>   
+                            </div>
+                            <div className='optionitems'>
+                               <input type="radio"/><label htmlFor="female">FEMALE</label>
+                            </div>
+                            
+                          </td>
+                      </tr>
+                      <tr className='createtablerow'>
+                          <td className='leftlabel'>Course </td>
+                          <td>:</td>
+                          <td className='coursecheckbox'>
+                              <div className='optionitems'>
+                                <input type="checkbox"/> <label htmlFor="MCA">MCA </label>
+                              </div>
+                              <div className='optionitems'>
+                                <input type="checkbox"/> <label htmlFor="BCA">BCA </label>
+                              </div>
+                              <div className='optionitems'>
+                                  <input type="checkbox"/> <label htmlFor="BSC">BSC </label>
+                              </div>
+                          </td>
+                      </tr>
+                      <tr className='createtablerow'>
+                          <td className='leftlabel'>Img Upload </td>
+                          <td>:</td>
+                          <td className='filetd'>
+                          <div className='imagefile'>
+                            <img src="" alt="img" />
+                          </div>
+                          <input className="loadfile" type="file" accept='.png, .jpg'/>
+                          </td>
+                      </tr>
 
+
+                      <tr className='createtablerow createbtnrow'>
+                          <td colSpan={3}><button className='submitbtn'>Update</button></td>
+                      </tr>
+                  </tbody>
+              </table>
+            </form>
+          </div>
+
+{/* //*********************************************** welcome part */}
           <div className='welcomediv'>
               <h1 className='welcomeheading'>Welcome to Admin Pannel</h1>
               <p>no Employee</p>
